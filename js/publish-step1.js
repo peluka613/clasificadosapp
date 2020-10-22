1e924afc2075140612fe4c2063f781d339315c9e
Julian Franco <julianfrancosolanilla@gmail.com>
2020-10-21 20:36:27 -0500

    publish-step1. Add text area character validation

--- js/publish-step1.js
+++ js/publish-step1.js
@@ -1,13 +1,43 @@
-document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
-    this.querySelector('.custom-select').classList.toggle('open');
-})
+const productDescription = document.querySelector('#productDescription');
 
-for (const option of document.querySelectorAll(".custom-option")) {
-    option.addEventListener('click', function() {
-        if (!this.classList.contains('selected')) {
-            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
-            this.classList.add('selected');
-            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
-        }
-    })
+// Listens all selectors 'custom-select-wrapper'
+document.querySelectorAll('.custom-select-wrapper').forEach(item => {
+    item.addEventListener('click', openSelector);
+});
+
+for (const option of document.querySelectorAll('.custom-option')) {
+    option.addEventListener('click', setSelectedOption)
+}
+
+// Listen typing on product description
+productDescription.addEventListener('keyup', productDescriptionHandler);
+
+/**
+ * Handle typing event
+ */
+function productDescriptionHandler() {
+    const charCounter = document.querySelector('.char-counter');
+
+    charCounter.textContent = `${productDescription.value.length} / 300`;
+}
+
+/**
+ * Open list of items
+ */
+function openSelector() {
+    this.querySelector('.custom-select').classList.toggle('open');    
+}
+
+/**
+ * Sets option chosen by the user
+ */
+function setSelectedOption() {
+    const optionSelected = this.parentNode.querySelector('.custom-option.selected');
+
+    if (!this.classList.contains('selected') && optionSelected) {
+        optionSelected.classList.remove('selected');
+    }
+
+    this.classList.add('selected');
+    this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;    
 }
