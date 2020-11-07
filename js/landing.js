9e25e1047a530522e21b6b17f47f16fe5cb68f93
Andrés Cataño <=>
2020-11-07 10:01:54 -0500

    Fixing issues for latest api

--- js/landing.js
+++ js/landing.js
@@ -5,68 +5,24 @@ function init() {
 async function getProducts() {
   toggleSpinner(true);
 
-  try {
-    const response = await fetch('http://localhost:3000/api/search/3', { method: 'GET'});
-    const products = await response.json();
-
-    setTimeout(() => {
-      toggleSpinner(false);
-      renderProducts(products);
-    }, 2000);
-  } catch (error) {
-    console.log(error);
-    toggleAlert(true);
-  }
-
-  // fetch('http://localhost:3000/api/search/3', { method: 'GET'})
-  //   .then(response => response.json())
-  //   .then(products => {
-  //     setTimeout(() => {
-  //       toggleSpinner(false);
-  //       renderProducts(products);
-  //     }, 2000)
-  //   })
-  //   .catch(error => {
-  //     console.log(error);
-  //     toggleAlert(true);
-  //   });
+  fetch('http://localhost:3000/api/search/latest/3', { method: 'GET'})
+   .then(response => response.json())
+   .then(products => {
+     setTimeout(() => {
+       toggleSpinner(false);
+       renderProducts(products);
+     }, 2000)
+   })
+   .catch(error => {
+     console.log(error);
+     toggleAlert(true);
+   });
 }
 
 function renderProducts(products) {
   let productsHtml = '';
   const productWrapper = document.getElementById('products-wrapper');
 
-  // for (let i=0; i<products.length; i++) {
-  //   productsHtml += `
-  //     <div class="product-card">
-  //       <img src="img/${products[i].FOTO}" alt="imagen producto">
-  //       <div class="product-card-text-container">
-  //         <h4>${products[i].NOMBRE}</h4>
-  //         <p>${products[i].DESCRIPCION}</p>
-  //       </div>
-  //     </div>
-  //   `;
-  // }
-
-  // BAD WAY
-  // products.forEach(product => {
-  //   const html = `
-  //   <div class="product-card">
-  //     <img src="img/${product.FOTO}" alt="imagen producto">
-  //     <div class="product-card-text-container">
-  //       <h4>${product.NOMBRE}</h4>
-  //       <p>${product.DESCRIPCION}</p>
-  //     </div>
-  //   </div>
-  //   `;
-
-  //   const productElement = document.createElement('div');
-
-  //   productElement.innerHTML = html;
-  //   productWrapper.appendChild(productElement);
-  // });
-
-  // GOOD WAY
   products.forEach(product => {
     productsHtml += `
       <div class="product-card">
