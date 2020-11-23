eff0b78e36d575b759bdb7b0d887c2c731461003
Carlos Cataño <serdnah2@gmail.com>
2020-11-23 12:39:08 -0500

    Adding missing styles, small css changes

--- js/results.js
+++ js/results.js
@@ -1,5 +1,7 @@
 const filterAPIURL =
   "http://ec2-18-220-72-102.us-east-2.compute.amazonaws.com:4001/api/search/filter";
+  const filterMeAPIURL =
+  "http://ec2-18-220-72-102.us-east-2.compute.amazonaws.com:4001/api/search/me";
 
 /**
  * Get the query params and get the products with filters
@@ -8,11 +10,17 @@ function getProducts() {
   const urlParams = new URLSearchParams(window.location.search);
   const categoryId = urlParams.get("category");
   const cityId = urlParams.get("city");
+  const myPublications = urlParams.get("me");
   const totalItems = document.querySelector("#total-items");
+  let url = `${filterAPIURL}/${cityId}/${categoryId}`;
+
+  if (myPublications && userData.id) {
+    url = `${filterMeAPIURL}/${userData.id}`;
+  }
 
   toggleSpinner(true);
 
-  fetch(`${filterAPIURL}/${cityId}/${categoryId}`, {
+  fetch(url, {
     method: "GET",
   })
     .then((response) => response.json())
